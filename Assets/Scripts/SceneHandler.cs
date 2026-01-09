68df515235ecd4662b518b2d8e2e237feb4ed23d
OskarDen1 <oskariangustavnilsson@gmail.com>
2026-01-09 08:47:48 +0100

    Removed Unwanted assets and replacing with "own" tilemap and a simple background made from this tilemap

---
+++ Assets/Scripts/SceneHandler.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+    public static SceneHandler Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            transform.SetParent(null);
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void LoadScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void LoadGameScene()
+    {
+        Scene scene = SceneManager.GetSceneByName("GameScene");
+        if (scene.isLoaded) return;
+        SceneManager.LoadScene("GameScene");
+    }
+}
---
+++ Assets/Scripts/dialogue.cs
@@ -0,0 +1,62 @@
+ using System.Collections;
+ using System.Collections.Generic;
+ using UnityEngine;
+ using TMPro;
+
+ public class Dialogue : MonoBehaviour
+ {
+     public TextMeshProUGUI textComponent;
+     public string[] lines;
+     public float textSpeed;
+     private int Index;
+     void Start()
+     {
+         textComponent.text = string.Empty;
+         StartDialogue();
+     }
+
+     void Update()
+     {
+         if (Input.GetMouseButton(0))
+         {
+             if (textComponent.text == lines[Index])
+             {
+                 NextLine();
+             }
+             else
+             {
+                 StopAllCoroutines();
+                 textComponent.text = lines[Index];
+             }
+         }
+     }
+
+     void StartDialogue()
+     {
+         Index = 0;
+         StartCoroutine(TypeLine());
+     }
+
+     IEnumerator TypeLine()
+     {
+         foreach (char c in lines[Index].ToCharArray())
+         {
+             textComponent.text += c;
+             yield return new WaitForSeconds(textSpeed);
+         }
+     }
+
+     void NextLine()
+     {
+         if (Index < lines.Length - 1)
+         {
+             Index++;
+             textComponent.text = string.Empty;
+             StartCoroutine(TypeLine());
+         }
+         else
+         {
+             gameObject.SetActive(false);
+         }
+     }
+ }
